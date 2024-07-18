aa402113980434b85f10fb59ff86367a00c04714
Ethan Lockwood <90814351+lockwood-ethan@users.noreply.github.com>
2024-07-18 17:11:51 -0400

    Forgot to save notes.js file before last commit

--- notes.js
+++ notes.js
@@ -39,16 +39,76 @@ console.log("Insert message here")
 %= | Mod by
 // When using compound operators wrap expressions after equals sign in parentheses:
 points = 2
-points *= 3 + 1 === points = points * (3 + 1)
+points *= 3 + 1 == points = points * (3 + 1)
 */
 
 // Convert strings to numbers
 parseInt("5") == 5
 parseFloat("5") == 5.0
-parseInt("dog") == Nan (Not a Number)
+parseInt("dog") == NaN (Not a Number)
 
 // Check if data type is NaN - Not a Number
 isNaN("5") == false
 isNaN("dog") == true
 
-// 
+// If statement structure
+if (condition) {
+    // Statements to execute when condition is true
+}
+
+// If-else statement structure
+if (condition) {
+    // Statements to execute when condition is true
+}
+else {
+    // Statements to execute when condition is false
+}
+
+// Curly braces are not required around if-else blocks with a single statement, but it is best practice to use them anyways
+if (vote == "M")
+    memberCount++;
+else
+    nonMemberCount++;
+
+// Nested if-else statement structure
+if (condition) {
+    if (condition) {
+        // Statements to execute when condition is true
+    }
+    else {
+        // Statements to execute when condition is false
+    }
+}
+else {
+    if (condition) {
+        // Statements to execute when condition is true
+    }
+    else {
+        // Statements to execute when condition is false
+    }
+}
+
+// Else-if statement structure, alternative to nested if/if-else statements
+if (condition) {
+    // Statements to execute when condition is true
+}
+else if (condition) {
+    // Statements to execute when condition is true
+}
+else {
+    // Statements to execute when condition is false
+}
+
+/* Comparison operators
+== | Equality
+!= | Inequality
+=== | Identity // "Strict" equality, only true if value AND data type are the same
+!== | Non-identity
+< | Less than
+<= | Less than or equal
+> | Greater than
+>= | Greater than or equal
+// Comparison operators convert strings to numbers automatically when comparing strings to numbers ex: "5" > 3 == true
+// If two strings containing letter characters are compared, the sum of the unicode number values are compared
+*/ 
+
