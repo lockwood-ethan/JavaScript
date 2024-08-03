c9fa429831e4d8323684741da57241b1849d3cdb
Ethan Lockwood <90814351+lockwood-ethan@users.noreply.github.com>
2024-08-03 12:06:53 -0400

    Basics of working with Document Object Model (DOM)

--- Javascript_With_HTML.js
+++ Javascript_With_HTML.js
@@ -64,3 +64,79 @@ processed until the webpage is completely loaded */
 /* Minification or minimization is the process of removing unnecessary characters (like whitespace and comments) from JavaScript code so the code
 executes the same but with fewer characters. */
 
+// Document Object Model
+
+/* The Document Object Model (DOM) is a data structure corresponding to the HTML document displayed in a web browser */
+
+// DOM Tree
+
+/* A DOM Tree is a visualization of DOM data structure */
+
+/* 
+A node is an individual object in the DOM tree. Nodes are created for each element, the text between an element's tags, and the element's attributes
+
+- The root node is the node at the top of the DOM
+- A child node is the node directly under another node. A node can have zero, one, or more child nodes (children
+- A parent node is the node directly above another node. All nodes, except the root node, have one parent node
+*/
+
+// Searching the DOM
+
+document.getElementById() // Returns the DOM node whose id attribute is the same as the method's parameter
+
+document.getElementsByTagName() // Returns an array of all the DOM nodes whose type is the same as the method's parameter
+
+document.getElementsByClassName() // Returns an array containing all the DOM nodes whose class attribute matches the method's parameter
+
+document.querySelectorAll() // Returns an array containing all the DOM nodes that match the CSS selector passed as the method's parameter
+
+document.querySelector() // Returns the first element found in the DOM that matches the CSS selector passed as the method's parameter
+
+/* 
+Technically, getElementsByTagName() and getElementsByClassName() return an HTMLCollection, and querySelectorAll() returns a NodeList
+
+HTMLCollection - An interface representing a generic collection of elements
+
+NodeList - An object with a collection of nodes
+
+HTMLCollection and NodeList both act like an array. Both have a length property and elements can be accessed with braces
+*/
+
+// Modifying DOM node attributes
+
+/* 
+JavaScript can perform actions including:
+
+- Change which image is displayed by modifying an img element's src attribute
+- Determine which image is currently displayed by reading the img element's src attribute
+- Change an element's CSS styling by modifying an element's style attribute
+
+Every attribute for an HTML element has an identically named property in the element's DOM node
+
+Example: 
+<a href="https://www.nasa.gov/" id="nasa_link">NASA</a> has a corresponding DOM node with properties names href and id. Each attribute property name
+acts as both a getter and a setter
+*/
+
+// Getter example
+let nasaURL = document.getElementById("nasa_link").href // Assigns nasaUrl the string "https://www.nasa.gov/" from the anchor element's href attribute
+
+// Setter example
+document.getElementById("nasa_link").href = "https://www.spacex.com/" // Changes the elements hyperlink to the SpaceX URL
+
+// An element's attribute can be removed using the element method removeAttribute()
+document.getElementById("nasa_link").removeAttribute("href") // Removes the link from the anchor element so that clicking on the HTML element no longer performs an action
+
+// Modifying DOM node content
+
+// The textContent property gets or sets a DOM node's text content
+document.querySelector("p").textContent = "$25.99"
+
+// The innerHTML property gets or sets a DOM node's content, including all of the node's children, using an HTML formatted string
+document.querySelector("p").innerHTML = "<strong>$25.99</strong>" // Changes paragraph to <p><strong>$25.99</strong><p>
+
+// nodeValue is a less common way to change node content
+document.getElementById("saleprice").firstChild.nodeValue = "$25.99" // firstChild accesses the textual content node for the element
+
+// innerText is an alternative to textContent that is aware of how text is rendered in the browser
+document.querySelector("p").innerText = "Testing     one" // Returns "Testing one" instead of "Testing    one"
---
+++ dom.js
@@ -0,0 +1,6 @@
+// Example of using JavaScript to search for elements in an HTML document
+let para = document.getElementById("early_languages");
+let listItems = document.querySelectorAll("li");
+
+console.log(para); 
+console.log(listItems); // Elements will show in the browser console
