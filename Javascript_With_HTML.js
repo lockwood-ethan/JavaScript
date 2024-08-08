8a77cbee3bda90fa8556bd4614f96e31e5e4a858
Ethan Lockwood <90814351+lockwood-ethan@users.noreply.github.com>
2024-08-08 19:06:07 -0400

    Event listeners and working with CSS

--- Javascript_With_HTML.js
+++ Javascript_With_HTML.js
@@ -140,3 +140,222 @@ document.getElementById("saleprice").firstChild.nodeValue = "$25.99" // firstChi
 
 // innerText is an alternative to textContent that is aware of how text is rendered in the browser
 document.querySelector("p").innerText = "Testing     one" // Returns "Testing one" instead of "Testing    one"
+
+// Accessing nodes
+
+// The JavaScript object document.documentElement is the root of the DOM tree
+let html = document.documentElement //Assigns the variable html with the document's root node
+ 
+document.documentElement.parentNode // Refers to the node's parent
+
+document.documentElement.childNodes // An array-like collection of objects for each node's children
+
+document.documentElement.children // Similar to the childNodes except the array contains only element nodes and no text nodes
+
+document.documentElement.nextElementSibling // Refers to the element node with the same parent following the current node in the document
+
+document.documentElement.previousElementSibling //  Refers to the element node with the same parent preceeding the current node in the document
+
+// Modifying the DOM structure
+
+// The appendChild() method appends a DOM node to the object's child nodes
+let ol = document.getElementsByTagName("ol")[0];
+let li = ol.getElementsByTagName("li")[0];
+ol.appendChild(li); // Moves the ordered list's first item to the last item of the same ordered list
+
+// The insertBefore() method inserts a DOM node as a child before an object's existing child node
+ol = document.getElementsByTagName("ol")[0];
+let items = ol.getElementsByTagName("li");
+ol.insertBefore(items[0], items[3]); // Moves the ordered list's first item before the fourth list item
+
+// The removeChild() method removes a node from the objects children
+ol = document.getElementsByTagName("ol")[0];
+items = ol.getElementsByTagName("li");
+items[0].parentNode.removeChild(items[0]); // Calls removeChild on the parent node of items[0] passing items[0] as the argument
+
+// Creating new nodes or duplicating existing nodes
+
+// The document method createElement() returns a new element node, created from a string argument that names the HTML element
+document.createElement("p") // creates a new paragraph element
+
+// The document method createTextNode() returns a new text node containing the text specified by a string argument
+document.createTextNode("Hello there!") // Creates the text node with "Hello there!", which can then be appended to an element node
+
+// The node method cloneNode() returns an identical copy of a DOM node. The methods boolean argument indicates whether the method should also clone the node's children
+x.cloneNode(true) // Creates an identical tree rooted at x
+x.cloneNode(false) // Creates a single node identical to x
+/* The cloneNode() method does not ensure that new nodes have unique id attributes */
+
+// Event-driven programming
+
+/* An event is an action, usually caused by a user, that the web browser responds to. Event-driven programming is a style where code runs only in response to various events, code that runs in response to an event is called an event handler or event listener
+
+// The following  are events for which web developers commonly write handlers:
+
+Change - Caused by an element value being modified
+
+Input - When the value of an input or textarea element is changed
+
+Load - When the browser completes loading a resource and dependent resources. Usually load is used with the body element to execute code once all the webpage's CSS, JavaScript, images, etc. have finished loading
+
+DOMContentLoad - When the HTML file has been loaded and parsed, although other related resources such as CSS, JavaScript, and image files may not yet be loaded
+
+Focus - When an element becomes the current receiver of keyboard input
+
+Blur - When an element loses focus and the element will no longer receive future keyboard input
+
+Submit - When the user submits a form to the web server
+*/
+
+// Handlers are written 3 ways
+
+<button onclick="clickHandler()">Click Me</button> // Embedding the handler as part of the HTML
+// Embedding a handler in HTML mixes content and functionality and should be avoided whenever possible
+
+document.querySelector("#myButton").onclick = clickHandler // Setting the DOM node event handler property directly using JavaScript
+// A disadvantage of this of using this method is that setting the property only allows one handler for that element to registered
+
+document.querySelector("#myButton").addEventListener("click", clickHandler) // Using the JavaScript addEventListener() method to register an event handler for a DOM object
+// Good practice is to use the addEventListener() method whenever possible
+
+// Capturing, at target, and bubbling phases
+
+/* When an event occurs, the browser follows a simple DOM traversal process to determine which handlers are relevant and need to be called. The DOM traversal process has three phases: capturing, at targetm and bubbling.
+
+Event capturing - The browser traverses the DOM tree from the root to the event target at each node, at each node calling any event-specific handlers that were explicitly registered for activation during the capturing phase
+
+At target - The browser calls all event-specific handlers registered on the target node
+
+Event bubbling - The browser traverses the DOM tree from the event target node back to the root node, at each node calling all event-specific handlers registered for the bubbling phase on the current node
+*/
+// Some events do not bubble, such as blur, focus, and change
+
+// Preventing default behavior
+
+/* The event capturing and bubbling process can be stopped using the stopPropogation() method on the event object provided to the handler. Once stopPropogation() is called, the browser stops the traversal process but still calls relevant registered handlers on the current node 
+
+Preventing a browser from using a built-in handler for an event can be done using the preventDefault() method
+*/
+
+// Example of preventDefault() method
+let passwordInput = document.querySelector("#password");
+passwordInput.addEventListener("keydown", preventSpaces);
+passwordInput.addEventListener("input", checkPassword);
+
+function preventSpaces(event) {
+    // Ignore space input
+    if (event.key === " ") {
+        event.preventDefault();
+    }
+}
+
+function checkPassword() {
+    let password = this.value;
+    let passwordStrength = 0;
+
+    if (password.length >= 6) {
+        passwordStrength++;
+    }
+    if (password.toLowerCase() !== password) {
+        passwordStrength++;
+    }
+    let containsDigit = false
+    for (let i = 0; i < password.length; i++) {
+        if (password[i] >= 0 && password[i] <= "9") {
+            containsDigit = true;
+        }
+    }
+    if (containsDigit) {
+        passwordStrength++;
+    }
+}
+
+// Timers
+
+/* A timer is a general name for techniques to execute JavaScript code after some amount of time has occured */
+
+// setTimeout() method takes two arguments: a function and a delay in milliseconds
+let timerId = setTimeout(showSpecial, 3000);
+
+function showSpecial() {
+    let special = document.getElementById("special");
+    special.style.display = "block";
+    setTimeout(hideSpecial, 10000); // Hides the special after 10 seconds
+}
+
+function hideSpecial() {
+    let special = document.getElementById("special");
+    special.style.display = "none";
+}
+// clearTimeout() method cancels the timeout created by setTimeout() so that it never shows on the screen
+
+// Intervals
+
+/* A web browser is able to execute a function repeatedly with a time delay between calls using setInterval(). The setInterval() method takes two arguments: a function and a time interval in milliseconds */
+
+function startMoving() {
+    ballImage = document.getElementById("ball");
+    timerId = setInterval(moveBall, 10);
+}
+
+function moveBall() {
+    let left = parseInt(ballImage.style.left);
+    ballImage.style.left = left + 5 + "px";
+}
+// clearInterval() method cancels the interval
+
+// Modifying CSS with JavaScript
+
+// Modifying an elements inline style 
+
+/* The CSS Object Model is a set of APIs that allow JavaScript to manipulate the CSS properties of a webpage */
+
+let elem = document.getElementById("mydiv");
+
+// The getPropertyValue() method returns the value of an element's CSS property or an empty string if the property is not set
+elem.style.getPropertyValue("color") // Gets the element's color value
+
+// The setProperty() method sets the value of an element's CSS property
+elem.style.setProperty("color", "blue") // Sets the element's color to blue
+
+// The removeProperty() method removes an element's CSS property
+elem.style.removeProperty("color") // Removes the element's color property
+
+// The style CSS properties can alternatively be accessed and modified using JavaScript property names
+elem.style.color = "blue"
+
+// CSS property names that have dashes are converted in property names that use camel case
+elem.style.backgroundColor // background-color
+
+// Modifying a style sheet
+
+/* The document.styleSheets object is a list of the stylesheets used in the webpage. Each stylesheet in document.stylesheets is a CSSStyleSheet object, which maintains a list of the stylesheet's CSS rules in the property called CSSRules */
+
+// Two CSSStyleSheet methods allow CSS rules to be added or removed
+
+// The insertRule() method inserts a new rule into the stylesheet
+document.styleSheets[0].insertRule("p { color: blue; }") // Inserts a new paragraph rule that makes the text color blue
+
+// The deleteRule() method deletes a rule at a given index number from the stylesheet
+document.styleSheets[0].deleteRule(0) // Deletes the first rule from the first stylesheet
+
+// CSS properties can be accessed, modified and removed using the same methods that are used when modifying CSS inline
+document.styleSheets[0].cssRules[0].style.setProperty("color", "blue")
+
+// Adding and removing classes
+
+/* Every DOM node has a classList property that lists the classes assigned to the node. The div created from <div class="account warning"> has a classList with items "account" and "warning". Methods exist to add and remove classList items */
+
+// The add() method adds a class to the node's classList
+elem.classList.add("mystyle") // adds the class mystyle to the element's list of classes
+
+// The remove() method removes a class from the node's classList
+elem.classList.remove("mystyle") // removes the class mystyle from the element's list of classes
+
+// The toggle() method adds the class to the node's classList if the class is not present. If the class is already present, the class is removed
+elem.classList.toggle("mystyle") // toggles the class mystyle on or off
+
+// A DOM node's classList can also be modified directly using the className property
+elem.className = "cat adopted" // Assigns the cat and adopted classes to the element and removes any previously assigned classes from the node
+/* All classes assigned to className are also added to the node's classList, using classList as often as possible is best practice */
+
--- file.js
+++ file.js
@@ -1,1 +1,30 @@
-alert("Click OK or press Enter");
+// alert("Click OK or press Enter");
+
+let list = document.getElementById("strikeList");
+
+// Register handler for event bubbling phase
+list.addEventListener("mouseover", function(e) {
+    e.target.style.color = "red";
+});
+
+// Register handler for event capturing phase
+list.addEventListener("mouseout", function(e) {
+    e.target.style.color = "black";
+}, true);
+
+// list.addEventListener("click", function(c) {
+//     window.open("https://www.twitch.tv/")
+// });
+
+let myDiv = document.getElementById("mydiv");
+let width = myDiv.style.getPropertyValue("width");
+width = parseInt(width) + 100;
+myDiv.style.setProperty("width", width + "px");
+myDiv.style.setProperty("background-color", "red");
+myDiv.style.removeProperty("width");
+myDiv.addEventListener("mouseover", function(m) {
+    m.target.style.color = "green";
+});
+myDiv.addEventListener("mouseout", function(m) {
+    m.target.style.color = "red";
+})
